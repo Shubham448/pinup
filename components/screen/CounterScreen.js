14eb00e870483a14be319053467c624feec29384
Sooraj <codewithsooraj@gmail.com>
2022-09-15 00:13:46 +0530

    made some changes

--- components/screen/CounterScreen.js
+++ components/screen/CounterScreen.js
@@ -3,28 +3,56 @@ import styles from "../../styles/screen/CounterScreen.module.css";
 import CountUp from "react-countup";
 const CounterScreen = () => {
   const [time, setTime] = useState(0);
-  const [play, setPlay] = useState(true);
+  const [animation, setAnimation] = useState("start");
 
   useEffect(() => {
-    let randomTime = Math.random() * 15;
+    let randomTime = Math.floor(Math.random() * (15 - 1 + 1)) + 1;
     setTime(randomTime);
-  }, [play]);
+  }, [animation]);
+
+  useEffect(() => {
+    document.getElementById("bike").style.animationDuration = time + "s";
+  }, [time]);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setAnimation("start");
+    }, 5000);
+  }, [animation]);
 
   return (
-    <div className={` mt-3  ${styles.screenMain}`}>
-      <div className={` rounded-3 ${styles.screenInner}`}>
-        <h2>
-          <CountUp
-            start={0}
-            end={time}
-            decimals={2}
-            duration={time}
-            onEnd={() => setPlay(play ? false : true)}
-          />
-          x
-        </h2>
-      </div>
-    </div>
+    <>
+      {animation === "start" ? (
+        <div class={styles.hero}>
+          <div class={styles.highway}></div>
+          <div class={styles.city}></div>
+          <div class={styles.bike}>
+            <img src="/images/bike.png" alt="" id="bike" />
+          </div>
+          <h2 className={styles.counterValue}>
+            <CountUp
+              start={0}
+              end={time}
+              decimals={2}
+              duration={time}
+              onEnd={() => {
+                setAnimation("end");
+              }}
+            />
+            x
+          </h2>
+        </div>
+      ) : (
+        <div className={` mt-3  ${styles.screenMain}`} id="bike">
+          <div
+            className={` rounded-3 ${styles.screenInner} d-grid text-center`}
+          >
+            <h3 className="text-danger">Congratulations!!</h3>
+            <h4 className="text-success">Your score is {time}</h4>
+          </div>
+        </div>
+      )}
+    </>
   );
 };
 
